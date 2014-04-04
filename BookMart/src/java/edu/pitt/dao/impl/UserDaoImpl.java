10facb1a5d87259cb61fe965596b5296c7f2269f
huw30 <huan0924@gmail.com>
2014-04-04 16:46:54 -0400

    Update UserDaoImpl.java

--- BookMart/src/java/edu/pitt/dao/impl/UserDaoImpl.java
+++ BookMart/src/java/edu/pitt/dao/impl/UserDaoImpl.java
@@ -3,69 +3,89 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.pitt.dao.impl;
+
 import edu.pitt.dao.ObjectDAO;
 import edu.pitt.domain.User;
 import edu.pitt.utils.JdbcUtils;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
-
 /**
  * User Dao Implementation class
+ *
  * @author huanwang
  * @version Apr 3. 2014
  */
-public class UserDaoImpl implements ObjectDAO<User>{
+public class UserDaoImpl implements ObjectDAO<User> {
 
     @Override
     public void add(User user) {
         try {
             QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
             String sql = "insert into Users (userID, userName, password, type) values(?,?,?,?);";
-            Object[] params = {user.getUserID(),user.getUserName(),user.getPassword(),user.getType()};
-            runner.update(sql, params);                       
-         } catch(Exception e) {
+            Object[] params = {user.getUserID(), user.getUserName(), user.getPassword(), user.getType()};
+            runner.update(sql, params);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public User find(int id) {
-             try {
-          
+        try {
+
             QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
             String sql = "select * from Users where id=?";
-            return (User)runner.query(sql, id, new BeanHandler(User.class));
-           
-        } catch(Exception e) {
+            return (User) runner.query(sql, id, new BeanHandler(User.class));
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        
+
     }
 
     @Override
     public List<User> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
+            String sql = "select * from Users;";
+
+            return (List<User>) runner.query(sql, new BeanListHandler(User.class));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
     }
 
     @Override
-    public void delete(User e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void delete(User user) {
+        try {
+            QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
+            String sql = "DELETE FROM Users WHERE userID = '" + user.getUserID() + "';";
+            runner.update(sql);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+
     }
 
     @Override
-    public void update(User e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void update(User user) {
+        try {
+            QueryRunner runner = new QueryRunner(JdbcUtils.getDataSource());
+            String sql = "update Users set userID=?, userName=?, password=?, type=?";
+            Object[] params = {user.getUserID(), user.getUserName(), user.getPassword(), user.getType()};
+            runner.update(sql, params);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-      
-   
-    
-    
 
-
-    
 }
