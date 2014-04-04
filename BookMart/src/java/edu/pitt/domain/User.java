dc81fc7d87bc6da210da867e0c4814d3e12f3341
huw30 <huan0924@gmail.com>
2014-04-03 20:42:16 -0400

    add user bean

---
+++ BookMart/src/java/edu/pitt/domain/User.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.pitt.domain;
+
+/**
+ *
+ * @author huanwang
+ */
+public class User {
+    private int userID;
+    private String userName;
+    private String password;
+    private String type;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+}
